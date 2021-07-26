4e728809df05716e5c44e1f725b168ebd2ab15c0
Keith Godlewski <68201365+godlewskiRNTH@users.noreply.github.com>
2021-07-26 18:25:55 +0000

    added input

--- main.py
+++ main.py
@@ -4,20 +4,35 @@ from turtle import Turtle, Screen
 t: Turtle = Turtle()
 s: Screen = Screen()
 
-#draw a square w/o loop
-t.forward(150)
-t.right(90)
-t.forward(150)
-t.right(90)
-t.forward(150)
-t.right(90)
-t.forward(150)
-t.right(90)
+'''request length from user in console
+sideLength: int = int(input('Enter a side length please: '))
+'''
+#pop up window for input
+sideLength: int = s.numinput('Input Window', 'Enter a side length please: ')
 
-#draw a square with a loop
+'''
+#draw a square w/o loop
+t.forward(sideLength)
+t.right(90)
+t.forward(sideLength)
+t.right(90)
+t.forward(sideLength)
+t.right(90)
+t.forward(sideLength)
+t.right(90)
+'''
+
+#draw a simple square with a loop
 for item in range(4):
-  t.forward(75)
-  t.right(90)
+    t.forward(sideLength-10)
+    t.right(90)
+
+#draw a square within a square with a loop
+while (sideLength > 10):
+  for item in range(4):
+    t.forward(sideLength-10)
+    t.right(90)
+  sideLength-=10
 
 
 #next step
